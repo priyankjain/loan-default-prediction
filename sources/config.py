3e2eabd96b30bbe878676797b2ea459da97bcb36
priyankjain <priyankjjain61@gmail.com>
2016-12-02 17:44:14 -0500

    Ignored big data files

--- sources/config.py
+++ sources/config.py
@@ -4,10 +4,10 @@
 ###############################################################################
 HOME_FOLDER = '..' #Relative to the folder from which code is being run
 					# Which is the code folder
-CODE_FOLDER = 'code'
-DATA_FOLDER = 'data'
+CODE_FOLDER = 'sources'
+DATA_FOLDER = 'dataset'
 CHARTS_FOLDER = 'reports'
-CLEAN_TRAINING_FILE = 'train_v2_clean.csv'
+CLEAN_TRAINING_FILE = 'train.csv'
 RAW_TRAINING_FILE = 'train_v2.csv'
 RAW_TESTING_FILE = 'test_v2.csv'
-CLEAN_TESTING_FILE = 'test_v2_clean.csv'
+CLEAN_TESTING_FILE = 'test.csv'
